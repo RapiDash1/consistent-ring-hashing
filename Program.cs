7bf2cf0f474f8073c3aae15b04484f921763d065
RapiDash <srivatsa.m11@gmail.com>
2020-11-25 22:59:00 +0530

    Updated README.md

--- Program.cs
+++ Program.cs
@@ -6,17 +6,9 @@ namespace ConsistentRingHashing
     {
         static void Main(string[] args)
         {
-            RingHashing ringHasing = new RingHashing();
-            ringHasing.addNode("Node1");
-            ringHasing.addNode("Node2");
-            ringHasing.addNode("Node3");
-            ringHasing.addNode("Node4");
-
-            Console.WriteLine(ringHasing.addKey("This"));
-            Console.WriteLine(ringHasing.addKey("is"));
-            Console.WriteLine(ringHasing.addKey("just"));
-            Console.WriteLine(ringHasing.addKey("a"));
-            Console.WriteLine(ringHasing.addKey("test"));
+            // RingHashing ringHasing = new RingHashing();
+            // ringHasing.addNode("Node1");
+            // ringHasing.addKey("Test");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# consistent-ring-hashing
+# Consistent-ring-hashing
+
+Consistent hashing is a special kind of hashing such that when a hash table is resized, only n/m keys need to be remapped on average where n is the number of keys and m is the number of slots.
+
+This is used for data partitioning in Cassandra, Raik and more.
+
+For more information check out - https://en.wikipedia.org/wiki/Consistent_hashing
+
+---
+
+## Built with
+- C#
+
+---
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+- Clone this repo.
+- Make sure you have .NET installed.
+- Execute the folowing command to run the console app - 
+    ```
+    dotnet run
+    ```
+
+---
+
+## Author
+- RapiDash1
